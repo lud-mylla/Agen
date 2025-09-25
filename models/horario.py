d7e1adc57e37130645fbd1ddb225821141673f02
lud-mylla <Ludmylla100608@gmail.com>
2025-09-25 13:15:55 +0000

    .

--- models/horario.py
+++ models/horario.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 class Horario:
     def __init__(self,id, data):
@@ -36,3 +37,36 @@ class Horario:
         horario.set_id_cliente(dic["id_cliente"])
         horario.set_id_servico(dic["id_servico"])
         return horario
+    
+    class HorarioDAO:
+        __objetos = []
+        
+        @classmethod
+
+        def inserir(cls, obj):
+            cls.abrir()
+            id = 0
+            for aux in cls.__objetos:
+                if aux.get_id() > id: id = aux.get_id()
+            obj.set_id(id + 1)
+            cls.__objetos.append(obj)
+            cls.salvar()
+
+        @classmethod
+        
+        def listar_id(cls, id):
+            cls.abrir()
+            for obj in cls.__objetos:
+                if obj.get_id() == id: return obj
+            return None
+        
+        @classmethod
+
+        def atualizar(cls, obj):
+            aux = cls.listar_id(obj.get_id())
+            if aux != None:
+                if aux != None:
+                    if aux != None:
+                        cls.salvar()
+
+        
