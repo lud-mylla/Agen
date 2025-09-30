4bfea5bfbd732404487281c25971319057f2392f
lud-mylla <Ludmylla100608@gmail.com>
2025-09-30 14:57:25 +0000

    coisa nova

--- models/horario.py
+++ models/horario.py
@@ -83,3 +83,14 @@ class Horario:
             try:
                 with open("horarios.json", mode ="r") as arquivo:
                     list_dic= json.load(arquivo)
+                    for dic in list_dic:
+                        obj = Horario.from_json(dic)
+                        cls.__objetos.append(obj)
+            except FileNotFoundError:
+                pass
+
+        @classmethod
+        def salvar(cls):
+            with open("horarios.json", mode ="w") as arquivo:
+                json.dump(cls.__objetos, arquivo, default = Horario.to_json)
+                
